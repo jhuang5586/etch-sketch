2ca8541ecd8015aad6ca77975613a76e8b356f99
James Huang <jhuang5586@gmail.com>
2022-04-16 13:08:07 -0400

    created clear button

--- script.js
+++ script.js
@@ -7,11 +7,17 @@ function turnBlack(e) {
 
 for (let i = 0; i < 256; i++) {
   const cell = document.createElement("div");
-  cell.style.width = "30px";
-  cell.style.height = "30px";
-  cell.style.background = "white";
-  cell.style.border = "1px solid black";
+  cell.classList.add("cell");
   cell.addEventListener("mouseenter", turnBlack);
-
   grid.appendChild(cell);
+}
+
+const clear = document.getElementById("clear");
+clear.addEventListener("click", clearBoard);
+
+function clearBoard() {
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => {
+    cell.style.background = "white";
+  });
 }
